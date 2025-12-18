0c7644e843e775dc34dab2d4f45bdeb0eb7ca7ff
Barshamagar123 <barshamagar@Barshas-MacBook-Air.local>
2025-12-18 16:35:08 +0545

    makes changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,126 +3,90 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const NewApp());
 }
-class NewApp extends StatelessWidget {
+class NewApp extends StatefulWidget {
   const NewApp({super.key});
 
+  @override
+  State<NewApp> createState() => _NewAppState();
+}
+
+class _NewAppState extends State<NewApp> {
+  // @override
+  bool isChangedShape=false;
+  int count=1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-       appBar: AppBar(
-          leading: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(child: Text("P"),),
-          ),
-          backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.purpleAccent,
+          title: Text("Barsha Apps"),
           centerTitle: true,
-          title: Text("Profile"),
-          actions: [
-            Icon(Icons.menu),
+         
+          actions:[
+            Icon(Icons.home),
+            Icon(Icons.search),
+            Icon(Icons.headphones_battery_outlined)
           ],
+        leading: CircleAvatar(child: Text("B")),
+      
         ),
      body: SingleChildScrollView(
-  child: Column(
-    children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(child: Image.asset("assets/image.png")),
-      ),
-
-      Text(
-        "Oleg Ivanov",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
+       child: Padding(
+         padding: const EdgeInsets.all(8.0),
+         child: Column(
+          spacing: 10,
+          children: [
+            // Container(height: 100,width:100, color:Colors.red),
+            //  Container(height: 100, width: 100, color:Colors.green),
+            //  Container(height:100, width:100,color:Colors.brown),
+            //  Container(
+            //       height: 100,
+            //       width: 100,
+       
+            //       decoration: BoxDecoration(
+            //         color: Colors.green,
+            //         borderRadius: BorderRadius.all(Radius.circular(10)),
+            //       ),
+            //     ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            spacing: 10,
+            children: [
+                for(int i=0; i<count; i++)
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: isChangedShape ?  BoxShape.circle
+                      : BoxShape.rectangle,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                 
+          ],),
         ),
-      ),
+        //  Image.asset("assets/image.png"),
+        Icon(Icons.add),
+       ElevatedButton(onPressed: () {
+       setState(() {
+       isChangedShape= !isChangedShape;
+       });
+       }, child: Text("login")),
+       OutlinedButton(onPressed: () {}, child: Text("Add Shape")),
+       TextButton(onPressed: () {
+       setState(() {
+         count ++;
+       });}, child: Text("Text button"))
+          ],
+         
+            //  ElevatedButton(onPressed: () {}, child: Text("login"))
 
-      Text("Product Designer"),
-
-      SizedBox(height: 12),
-
-      // 👇 Icons below the text
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.facebook),
-          SizedBox(width: 12),
-          Icon(Icons.facebook),
-          SizedBox(width: 12),
-          Icon(Icons.facebook),
-        ],
-      ),
-    ],
-  ),
-),
-
+         ),
+       ),
+     )
       )
     );
   }
 }
-
-
-     // Icon(Icons.facebook)
-// class HomePage extends StatelessWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.purpleAccent,
-//           title: Text("Barsha Apps"),
-//           centerTitle: true,
-         
-//           actions:[
-//             Icon(Icons.home),
-//             Icon(Icons.search),
-//             Icon(Icons.headphones_battery_outlined)
-//           ],
-//         leading: CircleAvatar(child: Text("B")),
-      
-//         ),
-//      body: SingleChildScrollView(
-//        child: Padding(
-//          padding: const EdgeInsets.all(8.0),
-//          child: Column(
-//           spacing: 10,
-//           children: [
-//             Container(height: 100,width:100, color:Colors.red),
-//              Container(height: 100, width: 100, color:Colors.green),
-//              Container(height:100, width:100,color:Colors.brown),
-//              Container(
-//                   height: 100,
-//                   width: 100,
-       
-//                   decoration: BoxDecoration(
-//                     color: Colors.green,
-//                     borderRadius: BorderRadius.all(Radius.circular(10)),
-//                   ),
-//                 ),
-//                 Container(
-//                   height: 100,
-//                   width: 100,
-       
-//                   decoration: BoxDecoration(
-//                     shape: BoxShape.circle,
-//                     color: Colors.yellow,
-//                   ),
-//                 ),
-       
-//          Image.asset("assets/image.png"),
-//        ElevatedButton(onPressed: () {}, child: Text("login"))
-
-//           ],
-         
-//             //  ElevatedButton(onPressed: () {}, child: Text("login"))
-
-//          ),
-//        ),
-//      )
-//       )
-//     );
-//   }
-// }
